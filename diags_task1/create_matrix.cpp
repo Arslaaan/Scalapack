264e652b9d9a033ff1770e723d42ac2a3a6b2a0f
arslan <aochirov@gmail.com>
2018-10-27 22:43:32 +0300

    fixed multiply

--- diags_task1/create_matrix.cpp
+++ diags_task1/create_matrix.cpp
@@ -5,11 +5,52 @@
 
 using namespace std;
 
-void function(int i,int j,double& real,double& img){
+/*
+ * 1 1 1 2 2 2
+ * 1 1 1 2 2 2
+ * 1 1 1 2 2 2
+ * 3 3 3 4 4 4
+ * 3 3 3 4 4 4
+ * 3 3 3 4 4 4
+ */
+void block(int i, int j, double& real, double& img) {
+  complex<double> res;
+  if((i>=3) && (j<3))
+    real = 3;
+  else{
+    if((i>=3) && (j>=3))
+      real = 4;
+    if((i<3) && (j>=3))
+      real = 2;
+    if((i<3) && (j<3))
+      real = 1;
+  }
+  /*
+  if(j>=3)
+    real = 2;
+  //res = 6*i + j;
+  */
+  img = 0;
+}
+
+/*
+ * 0  1  2  3  4  5
+ * 6  7  8  9  10 11
+ * 12 13 14 15 16 17
+ * 18 19 20 21 22 23
+ * 24 25 26 27 28 29
+ * 30 31 32 33 34 35
+ */
+void line(int i, int j, double& real, double& img) {
   complex<double> res;
   res = 6*i + j;
   real = res.real();
   img = res.imag();
+}
+
+
+void function(int i,int j,double& real,double& img){
+  line(i, j, real, img);
 }
 
 void create_file(size_t N,size_t M,char* str){
--- diags_task1/src/main.cpp
+++ diags_task1/src/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-class Matrix{
+class DistributedMatrix{
   complex<double>* data;
   int rows,cols;//размер матрицы
   int procrows,proccols;//размер сетки процессов
@@ -20,7 +20,7 @@ class Matrix{
   int blocksize;
   int* desc;
 public:
-  Matrix(int rows,int cols,int lrank,int lnproc,int lblocksize,int licon){
+  DistributedMatrix(int rows,int cols,int lrank,int lnproc,int lblocksize,int licon) {
     rank = lrank;
     nproc = lnproc;
     blocksize = lblocksize;
@@ -34,24 +34,17 @@ public:
       procrows = divB;
       proccols = divA;
     }
-    else{
+    else {
       procrows = divA;
       proccols = divB;
     }
-    //cout<<"rows="<<procrows<<" cols="<<proccols<<endl;
     Create();
-    /*
-    for(int i =0;i<9;++i)
-      if(rank == 1)
-        cout<<desc[i]<<" ";
-    */
   }
 
-  void Create(){
+  void Create() {
     int x,y;
     int root = 0;
-    Cblacs_gridinit(&icon, (char *) "Column", procrows, proccols);
-    Cblacs_gridinfo(icon, &x, &y, &myProcRow, &myProcCol);
+    Cblacs_gridinfo(icon, &x, &y, &myProcCol, &myProcRow);
     myProcRows = numroc_(&rows, &blocksize, &myProcRow, &root, &procrows);
     myProcCols = numroc_(&cols, &blocksize, &myProcCol, &root, &proccols);
     myProcRowsOffset = npreroc_(&rows, &blocksize, &myProcRow, &root, &procrows);
@@ -62,7 +55,7 @@ public:
     desc = descinit();
   }
 
-  void Populate(){
+  void Populate() {
     MPI_File thefile;
     MPI_Offset filesize;
     MPI_File_open(MPI_COMM_WORLD,"matrix",MPI_MODE_RDONLY,MPI_INFO_NULL,&thefile);
@@ -72,43 +65,85 @@ public:
       MPI_File_set_view(thefile,2*sizeof(double)*(cols*(myProcRowsOffset+i)+myProcColsOffset),MPI_DOUBLE,MPI_DOUBLE,"native",MPI_INFO_NULL);
       MPI_File_read(thefile,tmp_buf,2*myProcCols,MPI_DOUBLE,MPI_STATUS_IGNORE);
   		for (int j = 0; j < myProcCols; j++) {
-  			data[i*myProcCols + j] = complex<double>(tmp_buf[j*2],tmp_buf[j*2 + 1]);
+  			data[i*myProcCols + j] = complex<double>(tmp_buf[j*2],0);
   		}
       delete(tmp_buf);
   	}
   }
 
-  void Print(){
-    for(int i = 0;i < procrows;++i)
-      for(int j = 0;j < proccols;++j){
+  void Fill_diag() {
+    if(myProcCol != myProcRow) {
+      return;
+    }
+    for(int i = 0; i < myProcRows; i++) {
+      data[i*(myProcCols+1)] = i+myProcRowsOffset+1;
+    }
+  }
+
+  void Fill(complex<double> c) {
+    for (int i = 0; i < myProcRows; i++) {
+      for (int j = 0; j < myProcCols; j++) {
+        data[i*myProcCols + j] = c;
+      }
+    }
+  }
+
+  void Print() {
+    for(int i = 0; i < procrows; ++i) {
+      for(int j = 0; j < proccols; ++j) {
         MPI_Barrier(MPI_COMM_WORLD);
-        if(i*proccols + j == rank){
+        if(i*proccols + j == rank) {
           cout<<"coordinates of processes grid:"<<endl;
           cout<<"("<<myProcRow<<","<<myProcCol<<")"<<endl;
-          for(int x = 0;x < myProcRows;++x){
-            for(int y = 0;y < myProcCols;++y){
+          for(int x = 0; x < myProcRows; ++x) {
+            for(int y = 0; y < myProcCols; ++y) {
               cout<< "["<<data[x*myProcCols + y]<<"]";
             }
             cout<<endl;
           }
         }
       }
+    }
     MPI_Barrier(MPI_COMM_WORLD);
   }
 
-  complex<double>* getData(){
+  complex<double>* getData() {
     return data;
   }
 
-  int* getDesc(){
+  void setData(complex<double>* new_data) {
+    for(int i = 0; i < myProcRows; i++) {
+      for(int j = 0; j < myProcCols; j++) {
+        int idx = i*myProcCols + j;
+        data[idx] = new_data[idx + myProcRowsOffset*cols + myProcColsOffset];
+      }
+    }
+  }
+
+  int* getDesc() {
     return desc;
   }
 
-  void Multiply(Matrix& B, Matrix& C){
+  /*
+   * result: C=C+AB
+   * C изменится,
+   * C меняется во время процедуры, так что не использовать ее в качестве A или B
+   */
+  void Multiply(DistributedMatrix& B, DistributedMatrix& C) {
     complex<double> cdOne(1.0,0.0);
+    complex<double> cdZero(0.0, 0.0);
     int ione = 1;
+    if(rank == 0)
+      cout<<"--------------A--------------"<<endl;
+    Print();
+    if(rank == 0)
+      cout<<"--------------B--------------"<<endl;
+    B.Print();
 
-    pzgemm_((char*)"N", (char*)"N", &rows, &rows, &rows, &cdOne, data, &ione, &ione, desc, B.getData(), &ione, &ione, B.getDesc(), &cdOne, C.getData(), &ione, &ione, C.getDesc());
+    pzgemm_((char*)"N", (char*)"N", &rows, &rows, &rows,
+            &cdOne, data, &ione, &ione, desc,
+            B.getData(), &ione, &ione, B.getDesc(),
+            &cdZero, C.getData(), &ione, &ione, C.getDesc());
   }
 
   int* descinit() {
@@ -123,48 +158,88 @@ public:
       cout<<"DESC INIT ERROR\n";
     return descme;
   }
-/*
-  void createSVD(double *s, complex<double> *u, complex<double> *vt) {
+
+  void createSVD(DistributedMatrix& middle, DistributedMatrix& u, DistributedMatrix& vt) {
     // TODO test it
 
-    int *descu = descinit(u), *descvt = descinit(vt);
+    double* s = new double [rows];//
+    complex<double> singularValues[rows*cols];
 
-    int iZero = 0;
+    int iOne = 1;
     complex<double> *work = new complex<double>[1];
     int info = 0, lwork = -1;
     double *rwork = new double[1 + 4 * max(rows, cols)];
 
-    pzgesvd_(char* "V", char* "V", 
-      &rows, &cols, dataCopy, &iZero, &iZero, desc, s, 
-      u, &iZero, &iZero, descu,
-      vt, &iZero, &iZero, descvt,
-      work, lwork, rwork, &info);
-  }
-*/
+    pzgesvd_((char*) "V", (char*) "V",
+      &rows, &cols, data, &iOne, &iOne, desc, s,
+      u.getData(), &iOne, &iOne, u.getDesc(),
+      vt.getData(), &iOne, &iOne, vt.getDesc(),
+      work, &lwork, rwork, &info);
 
-  ~Matrix(){
+    lwork = (int) work[0].real();
+    delete[] work;
+    work = new complex<double>[lwork];
+
+    pzgesvd_((char*) "V", (char*) "V",
+      &rows, &cols, data, &iOne, &iOne, desc, s,
+      u.getData(), &iOne, &iOne, u.getDesc(),
+      vt.getData(), &iOne, &iOne, vt.getDesc(),
+      work, &lwork, rwork, &info);
+
+    //delete[] work, rwork;
+  }
+
+
+  ~DistributedMatrix() {
     //Cblacs_gridexit(icon);
     //Cblacs_exit(0);
   }
 
 };
 
+void test_mult(int rank, int nproc, int icon) {
+  DistributedMatrix A(6,6,rank,nproc,3,icon);
+  DistributedMatrix B(6,6,rank,nproc,3,icon);
+  DistributedMatrix C(6,6,rank,nproc,3,icon);
+  A.Populate();
+  B.Populate();
+  C.Fill(0);
+  A.Multiply(B,C);
+  C.Print();
+}
+
+void test_svd(int rank, int nproc, int icon) {
+  DistributedMatrix A(6,6,rank,nproc,3,icon);
+  DistributedMatrix B(6,6,rank,nproc,3,icon);
+  DistributedMatrix C(6,6,rank,nproc,3,icon);
+  DistributedMatrix S(6,6,rank,nproc,3,icon);
+  A.Fill_diag();
+  B.Fill(0);
+  C.Fill(0);
+
+  A.createSVD(S, B, C);
+
+  A.Print();
+  B.Print();
+  C.Print();
+  S.Print();
+}
+
 
 int main(int argc,char** argv){
+  // init
   int rank,nproc;
   MPI_Init(&argc, &argv);
   Cblacs_pinfo(&rank, &nproc);
   int icon = -2;
   Cblacs_get(-1, 0, &icon);
-  Matrix A(6,6,rank,nproc,3,icon);
-  Matrix B(6,6,rank,nproc,3,icon);
-  Matrix C(6,6,rank,nproc,3,icon);
-  A.Populate();
-  B.Populate();
-  //A.Print();
-  //B.Print();
-  A.Multiply(B,C);
-  //C.Print();
+  Cblacs_gridinit(&icon, (char *) "r", 2, 2);
+
+  test_mult(rank, nproc, icon);
+  //test_svd(rank, nproc, icon);
+
+  // exit
   //Cblacs_gridexit(icon);
+  MPI_Barrier(MPI_COMM_WORLD);
   Cblacs_exit(0);
 }
